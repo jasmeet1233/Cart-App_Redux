f077bb4d0a314afbc15265af339637e35fc29c39
jasmeet1233 <jazzratra@gmail.com>
2021-10-09 18:48:29 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
+// redux stuff
+import { createStore } from "redux";
+import { INCREASE, DECREASE, RESET } from "./actions";
+import { reducer } from "./reducer";
+
+const initialStore = {
+  cart: cartItems,
+  total: 0,
+  amount:0
+};
+
+const store = createStore(reducer, initialStore);
+
+function App() {
+  // cart setup
+
+  return (
+    <main>
+      <Navbar />
+      <CartContainer cart={cartItems} />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/reducer.jsx
@@ -0,0 +1,5 @@
+import { INCREASE, DECREASE, RESET } from "./actions";
+
+export const reducer = (state, action) => {
+  return state;
+};
